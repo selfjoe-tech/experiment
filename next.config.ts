b127a7b01f4f440ba2bb1a0991aadc6330e650a3
selfjoe-tech <oluwaseekijoseph@gmail.com>
2026-01-05 00:58:37 +0200

    removed redirect in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "upskirtcandy.com" }],
-        destination: "https://www.upskirtcandy.com/:path*",
-        permanent: true, // 308
-      },
-    ];
-  },
   
     matcher: ['/((?!api|_next/static|_next/image|sitemap\\.xml|sitemap/|robots\\.txt|favicon\\.ico).*)'],
 
